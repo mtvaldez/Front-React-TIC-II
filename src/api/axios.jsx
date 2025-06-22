137591cb8050ee98b64cf0cd203de9b78723f144
ftambler <federicotambler@gmail.com>
2025-06-22 20:24:04 -0300

    Unauthorized 401 status code interceptor

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -25,7 +25,6 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    // O invalid token
     if (error.response?.status === 401) {
       window.location.href = "/";
     }
